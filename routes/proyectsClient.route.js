d6814096808e3a83e8a4b83d0f9331099ad3f05f
Sebas33376 <sebadaniloria0@gmail.com>
2023-11-12 15:46:42 -0300

    first commit

--- routes/proyectsClient.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import * as controller from "../controllers/proyectsClients.controller.js"
-
-const proyectsClientRoute = Router();
-
-proyectsClientRoute.get("/client/:id/proyectsClient", controller.getProyectsByClient);
-
-export default proyectsClientRoute
